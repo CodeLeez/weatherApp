7b17c30cab7877a825e6fb84e92b7865dee6c673
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2024-12-18 10:10:17 +0800

    Inject the forecast HTML from JavaScript

--- src/script.js
+++ src/script.js
@@ -58,7 +58,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Wed", "Thur", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌞</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15&deg;</strong>
+      </div>
+      <div class="weather-forecast-temperature">9&deg;</div>
+    </div>
+  </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
+displayForecast();
